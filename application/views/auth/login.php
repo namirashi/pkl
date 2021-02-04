35f8c71c5e1b960adf9de20353698dc2a8979b37
namirashi <namirashifwahk@gmail.com>
2021-02-04 15:15:32 +0700

    PKL-MGS

---
+++ application/views/auth/login.php
@@ -0,0 +1,73 @@
+<div class="container">
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+        <div class="col-xl-10 col-lg-12 col-md-9">
+
+            <div class="card o-hidden border-0 shadow-lg my-5">
+
+                <!-- navbar -->
+                <nav class="navbar navbar-expand-md container">
+                    <a class="navbar-brand" href="#"><img src="assets\img\logo.png" alt="" width="70px"></a>
+
+                    <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+                        <ul class="navbar-nav">
+                            <li class="nav-item mr-30">
+                                <a class="nav-link" href="#"><button type="button" class="btn btn-primary contact">Masuk</button></a>
+                            </li>
+                        </ul>
+                </nav>
+
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                    <div class="col-lg-6">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">
+                                    Hasilkan uang sebagai Mitra Padipos
+                                </h1>
+                                <h6 class="h4 text-black-1000 mb-4">
+                                    Daftarkan diri anda sekarang
+                                </h6>
+                            </div>
+                            <form class="user">
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="text" class="form-control form-control-user" id="name" placeholder="Nama Anda">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="text" class="form-control form-control-user" id="nomer" placeholder="No. Hp">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control form-control-user" id="email" placeholder="Email">
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="password" class="form-control form-control-user" id="password1" placeholder="Password">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="password" class="form-control form-control-user" id="password2" placeholder="Ulang Password">
+                                    </div>
+                                </div>
+                                <button type="button" class="btn btn-primary btn-block">
+                                    Daftar Sekarang
+                                </button>
+                                <hr>
+                            </form>
+                            <hr>
+                            <div class="text-center">Sudah punya akun?
+                                <a class="small" href="forgot-password.html">login disini</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+</div>
