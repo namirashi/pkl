a3c4fc7decbccb890f97b9f582fd51f286b440fa
vendisaputra <vendisaputra@gmail.com>
2021-02-11 14:43:27 +0700

    fix login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -7,25 +7,29 @@ class Auth extends CI_Controller
     {
         parent::__construct();
         $this->load->library('form_validation');
+
+
+        $this->load->helper('email');
     }
 
     public function index()
     {
         $data['title'] = 'Hasilkan uang sebagai Mitra Padipos';
         $this->load->view('templates/auth_header');
-        $this->load->view('auth/login');
+        $this->load->view('auth/login', $data);
         $this->load->view('templates/auth_footer');
     }
+
     public function registration()
     {
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim |valid_email|is_unique[user.email]', [
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[user.email]', array(
             'is_unique' => 'This email has already registered!'
-        ]);
-        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+        ));
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', array(
             'matches' =>  'password dont match!',
             'min_lenght' => 'password too short!'
-        ]);
+        ));
         $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
 
         if ($this->form_validation->run() == false) {
@@ -35,7 +39,7 @@ class Auth extends CI_Controller
             $this->load->view('templates/auth_footer');
         } else {
 
-            $data = [
+            $data = array(
                 'name' => $this->input->post('name'),
                 'email' => $this->input->post('email'),
                 'image' => 'default.jpg',
@@ -43,10 +47,12 @@ class Auth extends CI_Controller
                 'role_id' => 2,
                 'is_active' => 1,
                 'date_created' => time()
-            ];
+            );
+
             $this->db->insert('user', $data);
             $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation! your account has been created. Please activate your account</div>');
             redirect('auth');
+
         }
     }
 }
--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -22,6 +22,7 @@
                             </div>
                             <div class="form-group">
                                 <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email Address" value="<?= set_value('email'); ?>">
+                                <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                             </div>
                             <div class=" form-group row">
                                 <div class="col-sm-6 mb-3 mb-sm-0">
