938bb555f10583c28a4f0799290d542f7666b267
namirashi <namirashifwahk@gmail.com>
2021-02-23 10:37:29 +0700

    pkl

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,9 +6,9 @@ class Admin extends CI_Controller
 
     public function index()
     {
-        $data['title'] = 'Dashboard';
-        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
+        $data['title'] = 'Dashboard';
+        $data['user'] = $this->db->get_where('user', array('email' => $this->session->userdata('email')))->row_array();
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
         $this->load->view('templates/topbar', $data);
